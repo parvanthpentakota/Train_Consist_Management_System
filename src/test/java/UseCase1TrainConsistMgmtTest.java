d2f25b8bee3f6d0f160e8c9c557c9dd11dabe9b2
Parvanth Pentakota <parvanthpentakota2316@gmail.com>
2026-04-16 23:35:42 +0530

    UC1 - Initialize Train Consist Management App with ArrayList and JUnit test

---
+++ src/test/java/UseCase1TrainConsistMgmtTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UseCase1TrainConsistMgmtTest {
+
+    @Test
+    void testInitialSize() {
+        List<String> list = new ArrayList<>();
+        assertEquals(0, list.size());
+    }
+}
